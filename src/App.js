d4ed9598747833aee8fe607dbb91c20a7ba1ec6d
Manish Govind Talmale <manishtalmale@gmail.com>
2023-07-14 00:02:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Nav from './components/Nav'
+import {Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Todo from './components/Todo';
+import Shopping from './components/Shopping';
+import Contact from './components/Contact';
+import ApiData from './components/ApiData';
+import FormValidation from './components/FormValidation';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav/>
+     <Routes>
+      <Route path='/home' element={<Home/>}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
+      <Route path='/todo' element={<Todo />}></Route>
+      <Route path='/cart' element={<Shopping />}></Route>
+      <Route path='/apidata' element={<ApiData />}></Route>
+      <Route path='/form' element={<FormValidation />}></Route>      
+     </Routes>
     </div>
   );
 }
